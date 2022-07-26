31ed251451497a53f38765f37ecc0f10ffe358ba
Mikhail <mk0602604@gmail.com>
2022-07-26 21:11:20 +0300

    Generate board module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
