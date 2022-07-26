3573c45c1882e87a630aac3afd5e894f5215b4f7
Mikhail <mk0602604@gmail.com>
2022-07-26 21:19:10 +0300

    Generate task module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [BoardModule],
+  imports: [BoardModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
