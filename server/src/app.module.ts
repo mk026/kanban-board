2ca6e7bf501e9acbcf06b55363b98278c5802662
Mikhail <mk0602604@gmail.com>
2022-07-27 21:39:12 +0300

    Generate auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardModule, TaskModule],
+  imports: [BoardModule, TaskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
