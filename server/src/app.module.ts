26168f059533f28fef446aed23857644ceb418a1
Mikhail <mk0602604@gmail.com>
2022-07-27 21:45:22 +0300

    Generate column module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
+import { ColumnModule } from './column/column.module';
 
 @Module({
-  imports: [BoardModule, TaskModule, AuthModule],
+  imports: [BoardModule, ColumnModule, TaskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
