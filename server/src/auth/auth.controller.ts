0b18bae239d2f56c2b80ca9117a375912f2c3255
Mikhail <mk0602604@gmail.com>
2022-07-28 22:17:36 +0300

    Add signup and signin handlers

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('/signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return 'Signup...';
+  }
+
+  signin() {
+    return 'Signin...';
+  }
+}
