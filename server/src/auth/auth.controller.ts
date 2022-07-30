0ab8cda6ca71f7dedd1915c7873e3e96e5e692f3
Mikhail <mk0602604@gmail.com>
2022-07-30 13:28:17 +0300

    Fix auth controller route paths

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -7,12 +7,12 @@ import { SignupCredentialsDto } from './dto/signup-credentials.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/signup')
+  @Post('signup')
   signup(@Body() signupCredentialsDto: SignupCredentialsDto) {
     return this.authService.signup(signupCredentialsDto);
   }
 
-  @Post('/signin')
+  @Post('signin')
   signin(@Body() signinCredentialsDto: SigninCredentialsDto) {
     return this.authService.signin(signinCredentialsDto);
   }
