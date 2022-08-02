eb48c3496a9008ec70a906b92fcf8b07671aef8f
Mikhail <mk0602604@gmail.com>
2022-08-02 12:49:03 +0300

    Add config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
@@ -8,7 +9,14 @@ import { ColumnModule } from './column/column.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BoardModule, ColumnModule, TaskModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BoardModule,
+    ColumnModule,
+    TaskModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
