58568b4f0060d480ecc39285b076529c385c648d
Mikhail <mk0602604@gmail.com>
2022-08-05 15:19:15 +0300

    Configure and register task entity

--- server/src/task/task.entity.ts
+++ server/src/task/task.entity.ts
@@ -1,5 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
   description: string;
 }
--- server/src/task/task.module.ts
+++ server/src/task/task.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskController } from './task.controller';
+import { Task } from './task.entity';
 import { TaskService } from './task.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TaskController],
   providers: [TaskService],
 })
