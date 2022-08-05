750074ae89ec87b46e51b58158f82496a000013c
Mikhail <mk0602604@gmail.com>
2022-08-05 16:56:25 +0300

    Configure and register section module

--- server/src/section/section.entity.ts
+++ server/src/section/section.entity.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Section {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
 }
--- server/src/section/section.module.ts
+++ server/src/section/section.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SectionController } from './section.controller';
+import { Section } from './section.entity';
 import { SectionService } from './section.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Section])],
   controllers: [SectionController],
   providers: [SectionService],
 })
