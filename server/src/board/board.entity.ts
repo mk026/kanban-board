a4e2c654a4bf81cf080b5554eb9e40eca62737e5
Mikhail <mk0602604@gmail.com>
2022-08-05 15:07:05 +0300

    Configure and register board entity

--- server/src/board/board.entity.ts
+++ server/src/board/board.entity.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Board {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
 }
--- server/src/board/board.module.ts
+++ server/src/board/board.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardController } from './board.controller';
+import { Board } from './board.entity';
 import { BoardService } from './board.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board])],
   controllers: [BoardController],
   providers: [BoardService],
 })
