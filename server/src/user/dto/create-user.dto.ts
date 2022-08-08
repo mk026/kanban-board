e59b8f3b05e22660c6a7a392a8d9fbbc7ee4d87c
Mikhail <mk0602604@gmail.com>
2022-08-08 23:56:04 +0300

    Add validation for create-user dto

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
