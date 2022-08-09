b79f35b81520d9e9bca3406144db56575fe487d6
Mikhail <mk0602604@gmail.com>
2022-08-09 22:49:19 +0300

    Inject user-service into auth-service

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { SigninCredentialsDto } from './dto/signin-credentials.dto';
 import { SignupCredentialsDto } from './dto/signup-credentials.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
   signup(signupCredentialsDto: SignupCredentialsDto) {
     return signupCredentialsDto;
   }
