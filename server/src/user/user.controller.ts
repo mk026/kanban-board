8aab8af479180a958ca511671b0b49b1e40dad72
Mikhail <mk0602604@gmail.com>
2022-08-15 13:28:59 +0300

    Add getUser route in user controller

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -19,6 +19,11 @@ export class UserController {
     return this.userService.getUsers();
   }
 
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUser(id);
+  }
+
   @Put()
   updateUser(@Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(updateUserDto);
