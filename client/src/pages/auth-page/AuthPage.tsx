d11e6a3094c52e4dcc48ac26a29b674c875daf90
Mikhail <mk0602604@gmail.com>
2022-09-07 16:01:55 +0300

    Add pages for auth, profile and boards

---
+++ client/src/pages/auth-page/AuthPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const AuthPage: FC = () => {
+  return <div>AuthPage</div>;
+};
+
+export default AuthPage;
---
+++ client/src/pages/boards-page/BoardsPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const BoardsPage: FC = () => {
+  return <div>BoardsPage</div>;
+};
+
+export default BoardsPage;
