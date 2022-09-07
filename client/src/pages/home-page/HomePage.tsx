aaf408f6c6bc3280030fc3942a3847c771d3c5ee
Mikhail <mk0602604@gmail.com>
2022-09-07 17:31:44 +0300

    Add home page

---
+++ client/src/pages/home-page/HomePage.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -3,8 +3,10 @@ import { ComponentType } from "react";
 import ProfilePage from "../pages/profile-page/ProfilePage";
 import AuthPage from "../pages/auth-page/AuthPage";
 import BoardsPage from "../pages/boards-page/BoardsPage";
+import HomePage from "../pages/home-page/HomePage";
 
 export enum Paths {
+  HOME_PATH = "/",
   BOARDS_PATH = "/boards",
   AUTH_PATH = "/auth",
   PROFILE_PATH = "/profile",
@@ -21,5 +23,6 @@ export const authRoutes: IRoute[] = [
 ];
 
 export const publicRoutes: IRoute[] = [
+  { path: Paths.HOME_PATH, Component: HomePage },
   { path: Paths.AUTH_PATH, Component: AuthPage },
 ];
