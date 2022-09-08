8e301a78e037dbec464ae4877c498951b1874bfc
Mikhail <mk0602604@gmail.com>
2022-09-08 20:40:08 +0300

    Render signin/signup forms in auth page

--- client/src/pages/auth-page/AuthPage.tsx
+++ client/src/pages/auth-page/AuthPage.tsx
@@ -1,7 +1,23 @@
-import { FC } from "react";
+import { Button } from "@mui/material";
+import { FC, useState } from "react";
+import SigninForm from "../../components/forms/signin-form/SigninForm";
+import SignupForm from "../../components/forms/signup-form/SignupForm";
 
 const AuthPage: FC = () => {
-  return <div>AuthPage</div>;
+  const [isSignin, setIsSignin] = useState(false);
+
+  const toggleAuthMode = () => setIsSignin((prev) => !prev);
+
+  const activeForm = isSignin ? <SigninForm /> : <SignupForm />;
+
+  return (
+    <>
+      {activeForm}
+      <Button onClick={toggleAuthMode}>
+        Switch to {isSignin ? "signup" : "signin"}
+      </Button>
+    </>
+  );
 };
 
 export default AuthPage;
