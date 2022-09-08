0b822f16606a47c1d609bc715af4f17ea76df775
Mikhail <mk0602604@gmail.com>
2022-09-08 21:00:01 +0300

    Add basic validation for signin form

--- client/src/components/forms/signin-form/SigninForm.tsx
+++ client/src/components/forms/signin-form/SigninForm.tsx
@@ -1,16 +1,31 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 const SigninForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const signinHandler = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(signinHandler)}>
       <label>
         Email
-        <input type="text" />
+        <input type="text" {...register("email", { required: true })} />
       </label>
+      {errors.email && <p>Email is a required field</p>}
       <label>
         Password
-        <input type="password" />
+        <input type="password" {...register("password", { required: true })} />
       </label>
+      {errors.password && <p>Password is a required field</p>}
+      <Button type="submit">Submit</Button>
     </form>
   );
 };
