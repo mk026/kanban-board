9bb8a817eb280b4848b5ea8a9ba13778b8dcdd06
Mikhail <mk0602604@gmail.com>
2022-09-09 16:56:21 +0300

    Refactor board-item component
    Use a button handler to navigate to the selected board

--- client/src/components/boards/board-item/BoardItem.tsx
+++ client/src/components/boards/board-item/BoardItem.tsx
@@ -1,18 +1,24 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button, Card, Typography } from "@mui/material";
 
 import { IBoard } from "../../../models/IBoard";
+import { Paths } from "../../../routes";
 
 export interface BoardItemProps {
   board: IBoard;
 }
 
 const BoardItem: FC<BoardItemProps> = ({ board }) => {
+  const navigate = useNavigate();
+
+  const selectBoardHandler = () => navigate(`${Paths.BOARDS_PATH}/${board.id}`);
+
   return (
     <Card variant="outlined">
       <Typography variant="h2">{board.title}</Typography>
       <Typography variant="body1">{board.description}</Typography>
-      <Button>Select board</Button>
+      <Button onClick={selectBoardHandler}>Select board</Button>
     </Card>
   );
 };
