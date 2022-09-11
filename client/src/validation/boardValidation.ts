f63ad5cadc5d7a3ef4efbb400498ad1b3d0c2370
Mikhail <mk0602604@gmail.com>
2022-09-11 23:36:21 +0300

    Add board title/description length validation

--- client/src/validation/boardValidation.ts
+++ client/src/validation/boardValidation.ts
@@ -6,6 +6,6 @@ export interface BoardFormValues {
 }
 
 export const boardValidationSchema = yup.object({
-  title: yup.string().required(),
-  description: yup.string().optional(),
+  title: yup.string().min(1).max(100).required(),
+  description: yup.string().max(500).optional(),
 });
