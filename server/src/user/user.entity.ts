5ee7e9d08f279856193d9f4ba00e1f8d281f8201
Mikhail <mk0602604@gmail.com>
2022-09-23 23:37:43 +0300

    Add unique email constraint to user entity

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
 @Entity()
+@Unique(['email'])
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
