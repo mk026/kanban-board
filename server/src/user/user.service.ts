fcabf014845f2b85ae67a6ea4e50781a36af0df8
Mikhail <mk0602604@gmail.com>
2022-09-24 22:41:46 +0300

    Check if email is already in use before signup

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SignupCredentialsDto } from 'src/auth/dto/signup-credentials.dto';
 import { Repository } from 'typeorm';
@@ -25,6 +29,12 @@ export class UserService {
   }
 
   async addUser(signupCredentialsDto: SignupCredentialsDto) {
+    const foundUser = this.userRepository.findOne({
+      where: { email: signupCredentialsDto.email },
+    });
+    if (foundUser) {
+      throw new ConflictException('Email already in use');
+    }
     const user = this.userRepository.create(signupCredentialsDto);
     await this.userRepository.save(user);
   }
