e8f3152cb9ac3e0290ca98b64c43897924dede11
Mikhail <mk0602604@gmail.com>
2022-09-30 23:39:55 +0300

    Add implementation for signin method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { Repository } from 'typeorm';
@@ -17,8 +21,19 @@ export class AuthService {
     return { user };
   }
 
-  signin(signinCredentialsDto: SigninCredentialsDto) {
-    return signinCredentialsDto;
+  async signin(signinCredentialsDto: SigninCredentialsDto) {
+    const { email, password } = signinCredentialsDto;
+    const foundUser = await this.userRepository.findOne({ where: { email } });
+    if (!foundUser) {
+      throw new UnauthorizedException('Incorrect email or password');
+    }
+    const isPasswordValid = bcryptjs.compareSync(password, foundUser.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Incorrect email or password');
+    }
+    return {
+      user: { name: foundUser.name, email: foundUser.email },
+    };
   }
 
   async createUser(signupCredentialsDto: SignupCredentialsDto) {
