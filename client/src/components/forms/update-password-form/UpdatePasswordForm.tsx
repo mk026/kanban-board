52c63420baab6fcb89a579f2dba729e73e31abed
Mikhail <mk0602604@gmail.com>
2022-10-05 15:09:04 +0300

    Refactor update-password-form jsx
    Use mui text-fields instead of inputs

--- client/src/components/forms/update-password-form/UpdatePasswordForm.tsx
+++ client/src/components/forms/update-password-form/UpdatePasswordForm.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
@@ -24,21 +24,27 @@ const UpdatePasswordForm: FC = () => {
 
   return (
     <form onSubmit={handleSubmit(updatePasswordHandler)}>
-      <label>
-        Old password
-        <input type="password" {...register("oldPassword")} />
-      </label>
-      {errors.oldPassword && <p>{errors.oldPassword.message}</p>}
-      <label>
-        New Password
-        <input type="password" {...register("password")} />
-      </label>
-      {errors.password && <p>{errors.password.message}</p>}
-      <label>
-        Confirm new Password
-        <input type="password" {...register("confirmPassword")} />
-      </label>
-      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+      <TextField
+        type="password"
+        label="Old password"
+        {...register("oldPassword")}
+        error={!!errors.oldPassword}
+        helperText={errors.oldPassword && errors.oldPassword.message}
+      />
+      <TextField
+        type="password"
+        label="New Password"
+        {...register("password")}
+        error={!!errors.password}
+        helperText={errors.password && errors.password.message}
+      />
+      <TextField
+        type="password"
+        label="Confirm new Password"
+        {...register("confirmPassword")}
+        error={!!errors.confirmPassword}
+        helperText={errors.confirmPassword && errors.confirmPassword.message}
+      />
       <Button type="submit">Submit</Button>
     </form>
   );
