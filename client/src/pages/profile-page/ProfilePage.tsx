f0ad3ca02b720734a92436c8a8b2aad79ecd3596
Mikhail <mk0602604@gmail.com>
2022-10-08 23:58:34 +0300

    Connect profile page with user-store

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -1,7 +1,10 @@
 import { FC } from "react";
+import { useStore } from "../../hooks/useStore";
 
 const ProfilePage: FC = () => {
-  return <div>ProfilePage</div>;
+  const { userStore } = useStore();
+
+  return <div>Profile of {userStore.user?.name}</div>;
 };
 
 export default ProfilePage;
