cc33a9098da137d1c1e890be387ecb0e9d3b2586
Mikhail <mk0602604@gmail.com>
2022-10-11 22:57:30 +0300

    Implement update user method

--- client/src/store/user/User.ts
+++ client/src/store/user/User.ts
@@ -1,5 +1,6 @@
 import { makeAutoObservable } from "mobx";
 
+import { UserDto } from "./dto/UserDto";
 import { UserStore } from "./UserStore";
 
 export class User {
@@ -8,7 +9,7 @@ export class User {
   email: string;
   store: UserStore;
 
-  constructor(store: UserStore, id: number, name: string, email: string) {
+  constructor(store: UserStore, { id, name, email }: UserDto) {
     makeAutoObservable(this);
     this.id = id;
     this.store = store;
--- client/src/store/user/UserStore.ts
+++ client/src/store/user/UserStore.ts
@@ -1,19 +1,43 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 
 import { RootStore } from "..";
 import { User } from "./User";
 import { UserDto } from "./dto/UserDto";
+import { UpdateUserDto } from "./dto/UpdateUserDto";
+import UserService from "../../services/UserService";
 
 export class UserStore {
   rootStore: RootStore;
   user: User | null = null;
+  isLoading: boolean = false;
+  error: unknown = null;
 
   constructor(rootStore: RootStore) {
     makeAutoObservable(this);
     this.rootStore = rootStore;
   }
 
-  setUser({ id, name, email }: UserDto) {
-    this.user = new User(this, id, name, email);
+  setUser(userDto: UserDto) {
+    this.user = new User(this, userDto);
+  }
+
+  async updateUser(updateUserDto: UpdateUserDto) {
+    this.isLoading = true;
+    try {
+      const { data } = await UserService.updateUser(updateUserDto);
+      const updatedUser = new User(this, data);
+      runInAction(() => {
+        this.user = updatedUser;
+      });
+      return data;
+    } catch (error) {
+      runInAction(() => {
+        this.error = error;
+      });
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
   }
 }
