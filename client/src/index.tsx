f36daec3bb4ca25a1c7d29a96a50e79142179369
Mikhail <mk0602604@gmail.com>
2022-10-11 21:53:44 +0300

    Use dnd-provider to enable dnd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import App from "./App";
 import StoreProvider from "./context/StoreContext";
@@ -11,11 +13,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
       <StoreProvider>
         <App />
       </StoreProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    </DndProvider>
+  </BrowserRouter>
 );
