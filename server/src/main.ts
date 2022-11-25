8c2816d81d21a121b7b0c7d7a5beba7c870ce534
Mikhail <mk0602604@gmail.com>
2022-11-25 23:04:47 +0300

    Use config service to set app port

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT'));
+  await app.listen(port);
 }
 bootstrap();
