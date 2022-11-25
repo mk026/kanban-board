fc084138a78a39738c25189b767fd87da0f00268
Mikhail <mk0602604@gmail.com>
2022-11-25 23:08:05 +0300

    Add validation for env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import typeOrmConfig from './config/typeorm.config';
+import { validate } from './config/env.validation';
 import { BoardModule } from './board/board.module';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
 import { SectionModule } from './section/section.module';
 import { UserModule } from './user/user.module';
-import typeOrmConfig from './config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, validate }),
     TypeOrmModule.forRootAsync({ useFactory: typeOrmConfig }),
     BoardModule,
     SectionModule,
