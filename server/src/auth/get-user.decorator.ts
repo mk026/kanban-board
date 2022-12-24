b2a4439a40b43bc992446c7c9823428278d85b45
Mikhail <mk0602604@gmail.com>
2022-12-24 16:48:17 +0300

    Create interface for authenticated requests

--- server/src/auth/get-user.decorator.ts
+++ server/src/auth/get-user.decorator.ts
@@ -1,8 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+import { AuthRequest } from './interfaces/auth-request.interface';
+
 export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): number => {
-    const request = ctx.switchToHttp().getRequest();
+    const request = ctx.switchToHttp().getRequest<AuthRequest>();
     return request.user;
   },
 );
