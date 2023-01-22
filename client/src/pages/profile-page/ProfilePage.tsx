3e7e6d74349a8ebc2ea12112e0e7c7764be5a282
Mikhail <mk0602604@gmail.com>
2023-01-22 23:53:58 +0300

    Render update-profile-form on profile-page

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -1,11 +1,18 @@
+import { Typography } from "@mui/material";
 import { FC } from "react";
 
+import UpdateProfileForm from "../../components/forms/update-profile-form";
 import { useStore } from "../../hooks/useStore";
 
 const ProfilePage: FC = () => {
   const { userStore } = useStore();
 
-  return <div>Profile of {userStore.user?.name}</div>;
+  return (
+    <>
+      <Typography>Profile of {userStore.user?.name}</Typography>
+      <UpdateProfileForm />
+    </>
+  );
 };
 
 export default ProfilePage;
