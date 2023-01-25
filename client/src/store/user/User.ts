8305dce56a8c79893d363c40e74c22fb07b2ecd7
Mikhail <mk0602604@gmail.com>
2023-01-25 23:55:45 +0300

    Add optional bio field in observable user class

--- client/src/store/user/User.ts
+++ client/src/store/user/User.ts
@@ -6,14 +6,16 @@ import { UserResponse } from "../../types/userTypes";
 export class User {
   id: number;
   name: string;
+  bio?: string;
   email: string;
   store: UserStore;
 
-  constructor(store: UserStore, { id, name, email }: UserResponse) {
+  constructor(store: UserStore, { id, name, bio, email }: UserResponse) {
     makeAutoObservable(this);
     this.id = id;
     this.store = store;
     this.name = name;
+    this.bio = bio;
     this.email = email;
   }
 }
