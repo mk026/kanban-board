3ac4b0d13c18336c5130552588e68b383325d1ce
Mikhail <mk0602604@gmail.com>
2023-06-21 22:17:29 +0300

    Refactor app-info component markup

--- client/src/components/app-info/AppInfo.tsx
+++ client/src/components/app-info/AppInfo.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import { Card, Typography } from "@mui/material";
 
 const AppInfo: FC = () => {
-  return <div>Welcome to React Kanban Board App</div>;
+  return (
+    <Card>
+      <Typography variant="body1">Welcome to React Kanban Board App</Typography>
+    </Card>
+  );
 };
 
 export default AppInfo;
