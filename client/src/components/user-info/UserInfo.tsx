0e3d7adc19113c3511676abbb9c6d8ba660bba19
Mikhail <mk0602604@gmail.com>
2023-07-30 23:34:12 +0300

    Add styles for user-info component

--- client/src/components/user-info/UserInfo.tsx
+++ client/src/components/user-info/UserInfo.tsx
@@ -3,13 +3,15 @@ import { Paper, Typography } from "@mui/material";
 
 import { User } from "../../store/user/User";
 
+import classes from "./UserInfo.module.scss";
+
 interface UserInfoProps {
   user: User;
 }
 
 const UserInfo: FC<UserInfoProps> = ({ user }) => {
   return (
-    <Paper>
+    <Paper className={classes.info}>
       <Typography>{user.name}</Typography>
       <Typography>{user.bio}</Typography>
       <Typography>{user.email}</Typography>
