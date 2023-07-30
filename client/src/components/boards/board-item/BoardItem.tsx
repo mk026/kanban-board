bf8db71b066ca671406905049b3f53626657b493
Mikhail <mk0602604@gmail.com>
2023-07-30 23:30:49 +0300

    Add styles for board-item component

--- client/src/components/boards/board-item/BoardItem.tsx
+++ client/src/components/boards/board-item/BoardItem.tsx
@@ -5,6 +5,8 @@ import { Button, Card, Typography } from "@mui/material";
 import { Paths } from "../../../routes";
 import { Board } from "../../../store/board/Board";
 
+import classes from "./BoardItem.module.scss";
+
 export interface BoardItemProps {
   board: Board;
 }
@@ -16,7 +18,7 @@ const BoardItem: FC<BoardItemProps> = ({ board }) => {
   const removeBoardHandler = () => board.remove();
 
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" className={classes.item}>
       <Typography variant="h2">{board.title}</Typography>
       <Typography variant="body1">{board.description}</Typography>
       <Button onClick={selectBoardHandler}>Select board</Button>
