ef418fac8d3b1167e6c6567a8fa757c0f6b532ef
Mikhail <mk0602604@gmail.com>
2023-07-31 23:34:09 +0300

    Add styles for app-info component

--- client/src/components/app-info/AppInfo.tsx
+++ client/src/components/app-info/AppInfo.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { Card, Typography } from "@mui/material";
 
+import classes from "./AppInfo.module.scss";
+
 const AppInfo: FC = () => {
   return (
-    <Card>
+    <Card className={classes.info}>
       <Typography variant="body1">Welcome to React Kanban Board App</Typography>
     </Card>
   );
