2c68a830472d46d35cca2a9ce56cf202e87217c1
Mikhail <mk0602604@gmail.com>
2023-08-01 19:30:57 +0300

    Add styles for auth-links component

--- client/src/components/auth-links/AuthLinks.tsx
+++ client/src/components/auth-links/AuthLinks.tsx
@@ -4,13 +4,20 @@ import { Button, Stack } from "@mui/material";
 
 import { Paths } from "../../routes";
 
+import classes from "./AuthLinks.module.scss";
+
 const AuthLinks: FC = () => {
   return (
     <Stack direction="row">
       <Button component={NavLink} to={Paths.AUTH_PATH} variant="outlined">
         Create Account
       </Button>
-      <Button component={NavLink} to={Paths.AUTH_PATH} variant="contained">
+      <Button
+        className={classes.signin}
+        component={NavLink}
+        to={Paths.AUTH_PATH}
+        variant="contained"
+      >
         Signin
       </Button>
     </Stack>
